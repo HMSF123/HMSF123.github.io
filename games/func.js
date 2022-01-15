0a8910c3d49e9487e0a242690d579081d953474d
HMSF123 <60055153+HMSF123@users.noreply.github.com>
2022-01-15 18:07:58 +0800

    Add files via upload

--- games/func.js
+++ games/func.js
@@ -153,7 +153,10 @@ function set_height()
 	let vh=window.innerHeight*0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-window.onload=set_height;
+window.onload=function(){
+	set_height();
+	setTimeout(function() {window.scrollTo(0, 10)}, 0);
+};
 window.onresize=function(){
 	set_height();
 	if(window.innerWidth<=580 && window.innerHeight>=400)
